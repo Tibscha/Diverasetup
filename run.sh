da2de4211fc31d1964ae3338254b9e0c23f6c4d8
Tibscha <72516618+Tibscha@users.noreply.github.com>
2024-05-07 19:05:40 +0200

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,18 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt install libfuse2 -y
+sudo apt install dbus notification-daemon -y
+sudo apt install xdotool -y
+cd /home/raspi
+sudo wget -c https://s3.live.divera247.de/public/software/monitor/DIVERA247-Monitor-1.1.4-arm64.AppImage
+sudo chmod a+x /DIVERA247-Monitor-1.1.4-arm64.AppImage
+
+echo "[D-BUS Service]
+Name=org.freedesktop.Notifications
+Exec=/usr/lib/notification-daemon/notification-daemon" > /usr/share/dbus-1/services/org.freedesktop.Notifications.service
+
+mkdir -p ~/.config/autostart
+
+echo "[Desktop Entry]
+Type=Application
+Exec=/Pfad/Zum/Monitor.AppImage"
