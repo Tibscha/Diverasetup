69c64062fce8971b86163f09caa1f34c9b22ee22
Tibscha <72516618+Tibscha@users.noreply.github.com>
2024-05-07 19:11:31 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@ sudo apt upgrade -y
 sudo apt install libfuse2 -y
 sudo apt install dbus notification-daemon -y
 sudo apt install xdotool -y
-cd /home/raspi
 sudo wget -c https://s3.live.divera247.de/public/software/monitor/DIVERA247-Monitor-1.1.4-arm64.AppImage
 mv DIVERA247-Monitor-1.1.4-arm64.AppImage Monitor.AppImage
 sudo chmod a+x /DIVERA247-Monitor-1.1.4-arm64.AppImage
